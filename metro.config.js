441e129a236a54dc6d740262ad4b9b60979102d1
tapasgiri9 <tapasgiri9@gmail.com>
2026-08-20 16:18:19 +0530

    fix cjs issue

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
+import { getDefaultConfig } from 'expo/metro-config';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+export default config;
